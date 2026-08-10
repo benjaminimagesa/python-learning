cb10c4be1133f839bbd19c025a18c921786216cd
Benjamini Magesa <benjaminimagesa2@gmail.com>
2026-08-10 21:37:20 +0300

    Add pandas import and create DataFrame for sample data

--- external_tools/01_imports.py
+++ external_tools/01_imports.py
@@ -1,4 +1,5 @@
 import math 
+import pandas as pd
 
 result = math.sqrt(81)
 print(result)
@@ -10,3 +11,11 @@ circumference = 2 * pi * radius
 print(circumference)
 circle_area = pi * pow(radius, 2)
 print(circle_area)
+
+data = {
+    "name": ["Benjac", "Jacbae", "Mamito"],
+    "age": [21, 20, 26],
+    "city": ["Dar es Salaam", "Moshi", "Arusha"]
+}
+df = pd.DataFrame(data)
+print(df)
