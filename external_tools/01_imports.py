091939ab9b9edbc5db0fdc761561ea6344651e57
Benjamini Magesa <benjaminimagesa2@gmail.com>
2026-08-10 20:00:15 +0300

    import for specific function

--- external_tools/01_imports.py
+++ external_tools/01_imports.py
@@ -2,3 +2,11 @@ import math
 
 result = math.sqrt(81)
 print(result)
+
+#import specific function
+from math import pow, sqrt, pi
+radius = 5
+circumference = 2 * pi * radius
+print(circumference)
+circle_area = pi * pow(radius, 2)
+print(circle_area)
